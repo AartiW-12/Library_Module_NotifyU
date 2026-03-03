38347116e88ca90e66258e15e3e1edb33cada44b
AartiW-12 <aartiwhaval12@gmail.com>
2026-03-03 19:01:11 +0530

    fix : frontend issue

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -14,12 +14,16 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+serviceWorkerRegistration.unregister(); // ← only change here
+
 reportWebVitals();
